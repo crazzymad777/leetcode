ca411928f6924ea1a2e0af9de23b1d0ced9c9a5d
Yury Moskov <aerobus.778@gmail.com>
2024-12-04 06:23:31 +0300

    New short solution

--- math/implement-rand10-using-rand7/Solution.java
+++ math/implement-rand10-using-rand7/Solution.java
@@ -5,33 +5,8 @@
  */
 class Solution extends SolBase {
     public int rand10() {
-        int x = rand7()*7;
-        int y = rand7();
-        int value = (x + y)-7;
-        if (value <= 40)
-        {
-            return (value % 10) + 1;
-        }
-
-        int cell = value%10;
-        int reminder = 9*7;
-        for (int i = 1; i < cell; i++)
-        {
-            reminder = 63-(70-reminder);
-        }
-
-        int d = rand7()*10;
-        if (d <= reminder)
-        {
-            return cell;
-        }
-        
-        int fateful = rand7();
-        if ((70-d)+fateful <= reminder)
-        {
-            return cell;
-        }
-        return cell+1;
+        final int high = 2401; // 7**4
+        final int needle = (rand7()-1)+(rand7()-1)*7+(rand7()-1)*49+(rand7()-1)*343;
+        return (needle / 241) + 1;
     }
 }
-
